b3bd93ad07c05b80ee49edaea4c5ff14dc8a4d0d
Aman Bhatt <amanbhatt0910@gmail.com>
2026-02-19 20:24:36 +0530

    Time: 10 ms (87.50%) | Memory: 46.6 MB (28.97%) - LeetSync

---
+++ 696-count-binary-substrings/count-binary-substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int prevCount = 0, currCount = 1;
+        int count = 0;
+
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == s.charAt(i-1)) {
+                currCount++;
+            } else {
+                prevCount = currCount;
+                currCount = 1;
+            }
+
+            if(currCount <= prevCount) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
